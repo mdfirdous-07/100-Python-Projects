bed31d8fa2c9ac9bddd64afe05d861536f96d034
Md Firdous <firdousmd000@gmail.com>
2026-02-16 16:35:06 +0530

    Welcome message generator in Project-01.py

---
+++ Project-01.py
@@ -0,0 +1,12 @@
+  # welcome message generator 
+  
+# step1 : ask for user details
+name=input("what is your name? ")
+hobby=input("what's your favourite hobby? ")
+# age=int(input("What is your age? "))
+
+# step2: generalized a personalized welcome message
+print("\n  WELCOME MESSAGE  \n")
+print(f"welcome to the world of python programming.")
+print(f"Its a great to know that you love {hobby}.")
+print(f"Get ready to build something amazing today Mr/Mrs {name}.")
