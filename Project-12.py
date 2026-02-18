cfb203d9eef3bfccd01b93ba4b930bacad006d4a
Md Firdous <firdousmd000@gmail.com>
2026-02-18 05:54:46 +0530

    Implement temperature conversion functions and menu

---
+++ Project-12.py
@@ -0,0 +1,57 @@
+#  Temperature Converter
+
+# step1:- Define conversion function
+def celsius_to_fahrenheit(celsius):
+    return (celsius * 9/5) + 32
+
+def celsius_to_kelvin(celsius):
+    return celsius + 273.15
+
+def fahrenheit_to_celsius(fahrenheit):
+    return (fahrenheit - 32) * 5/9
+
+def fahrenheit_to_kelvin(fahrenheit):
+    return (fahrenheit - 32) *5/9 + 273.15
+
+def kelvin_to_celsius(kelvin):
+    return kelvin - 273.15
+
+def kelvin_to_fahrenheit(kelvin):
+    return (kelvin - 273.15 ) * 9/5 + 32
+
+#  step2:- Display the menu
+def show_menu():
+    print("\n---Temperature Converter Menu---\n")
+    print("1. Celsius to fahrenheit & kelvin")
+    print("2. fahrenheit to Celsius & kelvin")
+    print("3. kelvin to Celsius & fahrenheit")
+    print("4. exit")
+
+# step3:- Main program loop
+while True:
+    show_menu()
+    choice = input("Enter your choice (1/2/3/4): ")
+    
+    if choice == "1":
+        celsius = float(input("Enter Temperature in Celsius: "))
+        print(f"Fahrenheit: {celsius_to_fahrenheit(celsius):.2f}")
+        print(f"Kelvin: {celsius_to_kelvin(celsius):.2f}")
+        
+    elif choice == "2":
+        fahrenheit = float(input("Enter temperature in fahrenheit: "))
+        print(f"celsius: {fahrenheit_to_celsius(fahrenheit):.2f}")
+        print(f"kelvin: {fahrenheit_to_kelvin(fahrenheit):.2f}")
+    
+    elif choice == "3":
+        kelvin = float(input("Enter Temperature in Kelvin: "))
+        print(f"celsius: {kelvin_to_celsius(kelvin):.2f}")
+        print(f"fahrenheit: {kelvin_to_fahrenheit(kelvin):.2f}")
+        
+    elif choice == "4":
+        print(" Exiting the program. Goodbye! ")
+        break 
+    
+    else:
+        print("Invalid choice. Please select a valid option.")
+        
+    
